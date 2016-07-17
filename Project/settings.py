7368f2f0f83f4e5a5bff7e199b97be2d74131379
MisaGu <vselochi4@yandex.ru>
2016-07-17 22:19:07 +0300

    added STATIC_ROOT

--- Project/settings.py
+++ Project/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, '/chess_tournament/static/')
 
 STATIC_URL = '/static/'
 
